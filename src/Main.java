32ca4fe3048c3615fad2248851dbea00807dcfe9
oleksandrsakalosh <orel.oleksander@gmail.com>
2023-02-25 19:47:37 +0100

    simple testing with auto generated inputs

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,32 @@
 import BST.*;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args){
-        AvlTree tree = new AvlTree();
+        AvlTree Tree = new AvlTree();
 
+        for(int i = 0; i < 50; i++)
+            Tree.insert(generateString());
+
+        Tree.print();
     }
+
+    static String generateString(){
+        int leftLimit = 97; // letter 'a'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 10;
+        Random random = new Random();
+        StringBuilder buffer = new StringBuilder(targetStringLength);
+        for (int i = 0; i < targetStringLength; i++) {
+            int randomLimitedInt = leftLimit + (int)
+                    (random.nextFloat() * (rightLimit - leftLimit + 1));
+            buffer.append((char) randomLimitedInt);
+        }
+        String generatedString = buffer.toString();
+
+        return generatedString;
+    }
+
 }
